5817f1406a1bf166e0529437b1df800be518665a
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2017-12-05 07:37:25 -0200

    on transform caller

--- src/js/index.js
+++ src/js/index.js
@@ -6,53 +6,135 @@ const escalaInitX = 150;
 const escalaInitY = 150;
 
 
-function getCanvasContext(canvasId) {
-  const canvasGameArea = document.getElementById(canvasId);
-  const ctx = canvasGameArea.getContext('2d');
-  return ctx;
+
+/* 
+const ctx = getCanvasContext('canvas-game-area');
+drawRect(ctx, translacaoInitX, translacaoInitX, escalaInitX, escalaInitX);
+
+
+let rotacaoX = document.getElementById('rotacaoX').value;
+let rotacaoY = document.getElementById('rotacaoY').value;
+let escalaX = document.getElementById('escalaX').value;
+let escalaY = document.getElementById('escalaY').value;
+
+
+ctx.fillRect(0, 0, escalaInitX, escalaInitX);
+
+document.querySelector('#botao-aplicar-transformacao')
+  .addEventListener('click', (event) => {
+
+    let translacaoX = document.getElementById('translacaoX').value;
+    let translacaoY = document.getElementById('translacaoY').value;
+
+    ctx.clearRect(0, 0, escalaInitX, escalaInitX);
+    ctx.fillRect(0, 0, escalaInitX, escalaInitX);
+    ctx.clearRect(0, 0, escalaInitX, escalaInitX);
+
+    ctx.fillRect(translacaoX, translacaoY, escalaInitX, escalaInitX);
+
+    ctx.translate(translacaoX, translacaoY)
+
+  })
+
+const botaoAplicar;
+const botaoReset;
+
+ */
+
+
+
+/* 
+var canvas;//o elemento canvas sobre o qual desenharemos
+var ctx;//o "contexto" da canvas que será utilizado (2D ou 3D)
+var dx = 5;//a tava de variação (velocidade) horizontal do objeto
+var dy = 5;//a tava de variação (velocidade) vertical do objeto
+var x = 250;//posição horizontal do objeto (com valor inicial)
+var y = 100;//posição vertical do objeto (com valor inicial)
+var WIDTH = 500;//largura da área retangular
+var HEIGHT = 200;//altura da área retangular
+
+
+function Desenhar() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+  ctx.fill();
 }
 
-function drawRect(ctx, coordX, coordY, scaleX, scaleY) {
-  return ctx.fillRect(coordX, coordY, scaleX, scaleY)
+function LimparTela() {
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.beginPath();
+  ctx.rect(0, 0, WIDTH, HEIGHT);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
 }
 
-function drawCircle() {
-
+function KeyDown(evt) {
+  console.log('sd')
+  switch (evt.keyCode) {
+    case 38:
+      if (y - dy > 0) {
+        y -= dy;
+      }
+      break;
+    case 40:
+      if (y + dy < HEIGHT) {
+        y += dy;
+      }
+      break;
+    case 37:
+      if (x - dx > 0) {
+        x -= dx;
+      }
+      break;
+    case 39:
+      if (x + dx < WIDTH) {
+        x += dx;
+      }
+      break;
+  }
 }
 
+function Atualizar() {
+  LimparTela();
+  Desenhar();
+}
 
-// 'canvas-game-area'
+function Iniciar() {
+  canvas = document.getElementById("canvas-game-area");
+  ctx = canvas.getContext("2d");
+  console.log(ctx)
+  return setInterval(Atualizar, 10);
+}
+ */
+/* window.addEventListener('keydown', KeyDown, true);
+*/
+// Iniciar(); 
 
-(function () {
-  const ctx = getCanvasContext('canvas-game-area');
-  drawRect(ctx, translacaoInitX, translacaoInitX, escalaInitX, escalaInitX);
 
-
-
+function onTransformacoes() {
   document.querySelector('#botao-aplicar-transformacao')
     .addEventListener('click', (event) => {
-      ctx.clearRect(translacaoInitX, translacaoInitX, escalaInitX, escalaInitX)
+      console.log('transformação')
 
-      ctx.fillRect(translacaoInitX+100, translacaoInitX, escalaInitX, escalaInitX)
+    });
+}
 
-      console.log(document);
-    })
+function onReset() {
+  document.querySelector('#botao-reset')
+    .addEventListener('click', (event) => {
+      console.log('reset')
 
+    });
+}
 
-  const translacaoX = document.getElementById('translacaoX');
-  const translacaoY = document.getElementById('translacaoY').value;
-  const rotacaoX = document.getElementById('rotacaoX').value;
-  const rotacaoY = document.getElementById('rotacaoY').value;
-  const escalaX = document.getElementById('escalaX').value;
-  const escalaY = document.getElementById('escalaY').value;
-  /* 
-  const botaoAplicar;
-  const botaoReset;
-  */
+function inciar() {
+  onTransformacoes();
+  onReset();
 
+}
 
-
-
-
-
+(function () {
+  inciar();
 })();
